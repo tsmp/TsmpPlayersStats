8f7855968faa1f8d4692f3d229b84cd580da4c11
Artem-Zaxler <mr.bocharoff2015@yandex.ru>
2023-07-05 13:36:05 +0300

    The design is fully completed. Added a dark theme, turns on from 20:00 to 8:00

--- frontend/js/theme_switcher.js
+++ frontend/js/theme_switcher.js
@@ -1,5 +1,13 @@
 let currentDate = new Date();
 if (currentDate.getHours() >= 20 || currentDate.getHours() < 8) {
-    console.log('Темная тема');
-} else
-    console.log('Светлая тема');
+    const style_script_block = document.getElementById("dark_preloader_style");
+    const style_block = document.createElement("style");
+    style_block.innerHTML = style_script_block.innerHTML;
+
+    for (var i = 0; i < style_script_block.attributes.length; i++) {
+        var attr = style_script_block.attributes[i];
+        style_block.setAttribute(attr.name, attr.value);
+    }
+    style_script_block.parentNode.replaceChild(style_block, style_script_block);
+    document.getElementById('link_dark_theme').href = '../css/dark_theme.css';
+}
